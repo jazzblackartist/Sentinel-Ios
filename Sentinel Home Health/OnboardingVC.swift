a5643a6b3a030aac8420efa9b90c49bdc152301e
Jasmine B <jazzblackartist@gmail.com>
2020-09-23 19:06:52 -0400

    Initial commit

--- Sentinel Home Health/OnboardingVC.swift
+++ Sentinel Home Health/OnboardingVC.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class OnboardingVC: UIViewController {
+    @IBOutlet weak var SignInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
